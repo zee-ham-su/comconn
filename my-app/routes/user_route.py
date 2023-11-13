c3582d25c2701c2fcca9683d4d8d07b265f55066
zee-ham-su <hsufiian@yahoo.com>
2023-11-13 18:17:54 +0000

    arrange

--- my-app/routes/user_route.py
+++ my-app/routes/user_route.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-import sys
-sys.path.append('/root/commcon/my-app/backend/v1/models')
 from flask import Blueprint, jsonify
-from storage import storage
-from user import User
-
+from models.storage.db_storage import DBStorage
+from models.user import User
 user_bp = Blueprint('user_bp', __name__)
+
 
 @user_bp.route('/users')
 def get_users():
-    users = storage.get_all(User)
+    users = DBStorage.get_all(User)
     user_data = [{'id': user.id, 'username': user.username,
                   'email': user.email} for user in users]
     return jsonify({'users': user_data})
