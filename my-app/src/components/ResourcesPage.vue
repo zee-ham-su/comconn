29c93260ac46c8e9381ab7ab4899538858293c82
zee-ham-su <hsufiian@yahoo.com>
2023-11-21 19:52:24 +0000

    landing page

--- my-app/src/components/ResourcesPage.vue
+++ my-app/src/components/ResourcesPage.vue
@@ -1,11 +1,10 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Community Resources</h1>
-    <ul>
-      <li v-for="resource in resources" :key="resource.id">
-        {{ resource.name }} - {{ resource.description }}
-      </li>
-    </ul>
+    <div class="resource-card" v-for="resource in resources" :key="resource.id">
+      <h2>{{ resource.name }}</h2>
+      <p>{{ resource.description }}</p>
+    </div>
   </div>
 </template>
 
@@ -26,16 +25,21 @@ export default {
 
 <style scoped>
 /* Add styling for your ResourcesPage */
+.container {
+  max-inline-size: 800px;
+  margin: 0 auto;
+}
+
+.resource-card {
+  background-color: #ffffff;
+  border: 1px solid #e1e1e1;
+  padding: 20px;
+  margin-block-end: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
 h1 {
   color: #42b983;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  margin-block-end: 10px;
 }
 </style>
--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -1,6 +1,17 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from '@/components/HelloWorld.vue';
+import ResourcesPage from '@/components/ResourcesPage.vue';
+
+
+
 const routes = [
-    { path: '/', component: Home },
+    { path: '/', component: HelloWorld },
     { path: '/resources', component: ResourcesPage },
-    { path: '/reviews', component: Reviews },
     // Add more routes as needed
 ];
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+});
+export default router;
